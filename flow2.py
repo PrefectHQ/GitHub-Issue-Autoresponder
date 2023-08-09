9fc2fe5d47c6f6a404155cca05d205a2beccd846
Rob Freedy <104510333+robfreedy@users.noreply.github.com>
2023-08-09 11:22:25 -0500

    Update flow2.py with response to issue summary

--- flow2.py
+++ flow2.py
@@ -2,9 +2,24 @@ import os
 
 import marvin
 from marvin import ai_fn
-from prefect import flow
+from prefect import flow, task
+
+marvin.settings.openai.api_key = "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"
+
+@ai_fn
+@task
+def marvin_response(issue_text: str) -> str:
+    """
+    Given the github issue text, provide a friendly suggestion for a work around in Prefect 2.0. 
+    If a work around does not exist, return a message saying that the team at Prefect is looking into it. 
+    """
+
 
 
 @flow()
-def main():
-    print("Hello, world!")
+def main(text : str):
+    return marvin_response(issue_text = text)
+
+if __name__ == "__main__":
+    r = main("My Prefect 2.0 flow is in a crashed state. What should I do?")
+    print(r)
